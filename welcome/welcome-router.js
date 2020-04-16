e8c4b4e988a20359251ea333cfe731a68e6705f1
Virginia Wilcox <wilcoxva@gmail.com>
2020-04-15 20:32:17 -0400

    changes

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -1,10 +1,13 @@
+// require("dotenv").config()
+
 const express = require("express")
 
 const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		message: "Welcome",
+		message: `Welcome ${process.env.COHORT}`,
+		face: process.env.FUN_FACT || "I  have no fun facts",
 	})
 })
 
